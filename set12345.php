03dc0318927360bbf76d50fdb4ab98a2761527ae
Harish Naidu <44311130+harishnaidumaddala@users.noreply.github.com>
2019-07-02 22:09:19 +0530

    modified
    improved some css

--- set12345.php
+++ set12345.php
@@ -55,6 +55,16 @@ div{
 
 <!-------------------------------------------------------------form--------------------------------------------------------------------------->
 <style>
+#question {
+	background-color: white;
+	 width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
 body {
     background-color: lightblue;
 }
@@ -88,7 +98,7 @@ input[type=submit] {
 
 input[type=submit]:hover {
     background-color: #45a049;
-
+}
 
 </style>
 <h1 align="center">online competetive</h1>
@@ -136,78 +146,79 @@ input[type=submit]:hover {
 	<!----<form  action="testset.php" method="post">  -->
 	
 	
-
-		<p class="question1">1.Which keyword can be used for coming out of recursion?</p>
-			<ul class="answers1">
+		
+		<p id="question" class="question1">1.Which keyword can be used for coming out of recursion?</p>
+			<ul  id="question" id="question" class="answers1">
 				<input type="radio" name="q1" value="1" id="q1a"><label for="q1a">break</label><br/>
 				<input type="radio" name="q1" value="2" id="q1b"><label for="q1b">return</label><br/>
 				<input type="radio" name="q1" value="3" id="q1c"><label for="q1c">exit</label><br/>
 				<input type="radio" name="q1" value="4" id="q1d"><label for="q1d"> both break and return</label><br/>
 			</ul> 
+		
 
 
-		<p class="question2">2.Which among the following is wrong for “register int a;”?  </p>
-			<ul class="answers2">
+		<p id="question" class="question2">2.Which among the following is wrong for “register int a;”?  </p>
+			<ul id="question" class="answers2">
 					<input type="radio" name="q2" value="1" id="q2a"><label for="q2a">Compiler generally ignores the request</label><br/>
 					<input type="radio" name="q2" value="2" id="q2b"><label for="q2b">You cannot take the address of this variable</label><br/>
 					<input type="radio" name="q2" value="3" id="q2c"><label for="q2c">Access time to a is critical</label><br/>
 					<input type="radio" name="q2" value="4" id="q2d"><label for="q2d">None of the mentioned</label><br/>
 			</ul> 
 
-		<p class="question3">3. Register storage class can be specified to global variables.</p>
-			<ul class="answers3">
+		<p id="question" class="question3">3. Register storage class can be specified to global variables.</p>
+			<ul id="question" class="answers3">
 				<input type="radio" name="q3" value="1" id="q3a"><label for="q3a">True</label><br/>
 				<input type="radio" name="q3" value="2" id="q3b"><label for="q3b">False</label><br/>
 				<input type="radio" name="q3" value="3" id="q3c"><label for="q3c">Depends on the compiler</label><br/>
 				<input type="radio" name="q3" value="4" id="q3d"><label for="q3d">Depends on the standard</label><br/>
 			</ul> 
 
-		<p class="question4">4.register keyword mandates compiler to place it in machine register.</p>
-			<ul class="answers4">
+		<p id="question" class="question4">4.register keyword mandates compiler to place it in machine register.</p>
+			<ul id="question" class="answers4">
 				<input type="radio" name="q4" value="1" id="q4a"><label for="q4a">True</label><br/>
 				<input type="radio" name="q4" value="2" id="q4b"><label for="q4b">False</label><br/>
 				<input type="radio" name="q4" value="3" id="q4c"><label for="q4c">Depends on the standard</label><br/>
 				<input type="radio" name="q4" value="4" id="q4d"><label for="q4d"> None of the mentioned</label><br/>
 			</ul> 
-		<p class="question5">5. What is the format identifier for “static a = 20.5;”?</p>
-			<ul class="answers5">
+		<p id="question" class="question5">5. What is the format identifier for “static a = 20.5;”?</p>
+			<ul id="question" class="answers5">
 				<input type="radio" name="q5" value="1" id="q5a"><label for="q5a">%s</label><br/>
 				<input type="radio" name="q5" value="2" id="q5b"><label for="q5b">%d</label><br/>
 				<input type="radio" name="q5" value="3" id="q5c"><label for="q5c"> %f</label><br/>
 				<input type="radio" name="q5" value="4" id="q5d"><label for="q5d"> Illegal declaration due to absence of data type</label><br/>
 			</ul> 
-		<p class="question6">6.Which of the following is true for static variable?</p>
-			<ul class="answers6">
+		<p id="question" class="question6">6.Which of the following is true for static variable?</p>
+			<ul id="question" class="answers6">
 				<input type="radio" name="q6" value="1" id="q6a"><label for="q6a">It can be called from another function</label><br/>
 				<input type="radio" name="q6" value="2" id="q6b"><label for="q6b">It exists even after the function ends</label><br/>
 				<input type="radio" name="q6" value="3" id="q6c"><label for="q6c">It can be modified in another function by sending it as a parameter</label><br/>
 				<input type="radio" name="q6" value="4" id="q6d"><label for="q6d"> All of the mentioned</label><br/>
 			</ul> 
-		<p class="question7">7.Assignment statements assigning value to local static variables are executed only once.</p>
-			<ul class="answers7">
+		<p id="question" class="question7">7.Assignment statements assigning value to local static variables are executed only once.</p>
+			<ul id="question" class="answers7">
 				<input type="radio" name="q7" value="1" id="q7a"><label for="q7a">True</label><br/>
 				<input type="radio" name="q7" value="2" id="q7b"><label for="q7b">False</label><br/>
 				<input type="radio" name="q7" value="3" id="q7c"><label for="q7c">Depends on the code</label><br/>
 				<input type="radio" name="q7" value="4" id="q7d"><label for="q7d">None of the mentioned</label><br/>
 			</ul> 
-		<p class="question8">8.Is initialisation mandatory for local static variables?</p>
-			<ul class="answers8">
+		<p id="question" class="question8">8.Is initialisation mandatory for local static variables?</p>
+			<ul id="question" class="answers8">
 				<input type="radio" name="q8" value="1" id="q8a"><label for="q8a"> Yes</label><br/>
 				<input type="radio" name="q8" value="2" id="q8b"><label for="q8b"> No</label><br/>
 				<input type="radio" name="q8" value="3" id="q8c"><label for="q8c"> Depends on the compiler</label><br/>
 				<input type="radio" name="q8" value="4" id="q8d"><label for="q8d"> Depends on the standard</label><br/>
 			</ul> 
 
-		<p class="question9">9. Functions have static qualifier for its declaration by default.</p>
-			<ul class="answers9">
+		<p id="question" class="question9">9. Functions have static qualifier for its declaration by default.</p>
+			<ul id="question" class="answers9">
 				<input type="radio" name="q9" value="1" id="q9a"><label for="q9a">True</label><br/>
 				<input type="radio" name="q9" value="2" id="q9b"><label for="q9b">False</label><br/>
 				<input type="radio" name="q9" value="3" id="q9c"><label for="q9c">Depends on the compiler</label><br/>
 				<input type="radio" name="q9" value="4" id="q9d"><label for="q9d"> Depends on the standard</label><br/>
 			</ul> 
-		<p class="question10">10. Which of the following cannot be static in C?
+		<p  id="question" class="question10">10. Which of the following cannot be static in C?
 </p>
-			<ul class="answers10">
+			<ul id="question" class="answers10">
 				<input type="radio" name="q10" value="1" id="q10a"><label for="q10a">Variables</label><br/>
 				<input type="radio" name="q10" value="2" id="q10b"><label for="q10b">Functions</label><br/>
 				<input type="radio" name="q10" value="3" id="q10c"><label for="q10c">Structures</label><br/>
@@ -215,8 +226,8 @@ input[type=submit]:hover {
 			</ul> 
 
 
-		<p class="question11">11.Which of following is not accepted in C?</p>
-			<ul class="answers11">
+		<p id="question" class="question11">11.Which of following is not accepted in C?</p>
+			<ul id="question" class="answers11">
 				<input type="radio" name="q11" value="1" id="q11a"><label for="q11a">static a = 10; //static as</label><br/>
 				<input type="radio" name="q11" value="2" id="q11b"><label for="q11b">static int func (int); //parameter as static</label><br/>
 				<input type="radio" name="q11" value="3" id="q11c"><label for="q11c">static static int a; //a static variable prefixed with static</label><br/>
@@ -224,68 +235,67 @@ input[type=submit]:hover {
 			</ul> 
 
 
-		<p class="question12">12.What is the scope of a function?</p>
-			<ul class="answers12">
+		<p id="question" class="question12">12.What is the scope of a function?</p>
+			<ul id="question" class="answers12">
 				<input type="radio" name="q12" value="1" id="q12a"><label for="q12a">Whole source file in which it is defined</label><br/>
 				<input type="radio" name="q12" value="2" id="q12b"><label for="q12b">From the point of declaration to the end of the file in which it is defined</label><br/>
 				<input type="radio" name="q12" value="3" id="q12c"><label for="q12c">Any source file in a program</label><br/>
 				<input type="radio" name="q12" value="4" id="q12d"><label for="q12d"> From the point of declaration to the end of the file being compiled</label><br/>
 			</ul> 
 
-		<p class="question13">13.What is the scope of an external variable?</p>
-			<ul class="answers13">
+		<p id="question" class="question13">13.What is the scope of an external variable?</p>
+			<ul id="question" class="answers13">
 				<input type="radio" name="q13" value="1" id="q13a"><label for="q13a">Whole source file in which it is defined</label><br/>
 				<input type="radio" name="q13" value="2" id="q13b"><label for="q13b"> From the point of declaration to the end of the file in which it is defined</label><br/>
 				<input type="radio" name="q13" value="3" id="q13c"><label for="q13c">Any source file in a program</label><br/>
 				<input type="radio" name="q13" value="4" id="q13d"><label for="q13d">From the point of declaration to the end of the file being compiled</label><br/>
 			</ul> 
 
-		<p class="question14">14. What is the scope of an external variable?</p>
-			<ul class="answers14">
+		<p id="question" class="question14">14. What is the scope of an external variable?</p>
+			<ul id="question" class="answers14">
 				<input type="radio" name="q14" value="1" id="q14a"><label for="q14a">Whole source file in which it is defined</label><br/>
 				<input type="radio" name="q14" value="2" id="q14b"><label for="q14b"> From the point of declaration to the end of the file in which it is defined</label><br/>
 				<input type="radio" name="q14" value="3" id="q14c"><label for="q14c"> Any source file in a program</label><br/>
 				<input type="radio" name="q14" value="4" id="q14d"><label for="q14d">From the point of declaration to the end of the file being compiled</label><br/>
 			</ul> 
-		<p class="question15">15.Property of the external variable to be accessed by any source file is called by the C90 standard as ____</p>
-			<ul class="answers15">
+		<p id="question" class="question15">15.Property of the external variable to be accessed by any source file is called by the C90 standard as ____</p>
+			<ul id="question" class="answers15">
 				<input type="radio" name="q15" value="1" id="q15a"><label for="q15a">external linkage</label><br/>
 				<input type="radio" name="q15" value="2" id="q15b"><label for="q15b">external scope</label><br/>
 				<input type="radio" name="q15" value="3" id="q15c"><label for="q15c"> global scope</label><br/>
 				<input type="radio" name="q15" value="4" id="q15d"><label for="q15d"> global linkage</label><br/>
 			</ul> 
-		<p class="question16">16.What is the return-type of the function sqrt()? </p>
-			<ul class="answers16">
+		<p id="question" class="question16">16.What is the return-type of the function sqrt()? </p>
+			<ul id="question" class="answers16">
 				<input type="radio" name="q16" value="1" id="q16a"><label for="q16a"> int</label><br/>
 				<input type="radio" name="q16" value="2" id="q16b"><label for="q16b"> float</label><br/>
 				<input type="radio" name="q16" value="3" id="q16c"><label for="q16c"> double</label><br/>
-				<input type="radio" name="q16" value="4" id="q16d"><label for="q16d">depends on the data type of the parameter
-</label><br/>
+				<input type="radio" name="q16" value="4" id="q16d"><label for="q16d">depends on the data type of the parameter</label><br/>
 			</ul> 
-		<p class="question17">17. goto can be used to jump from main() to within a function.</p>
-			<ul class="answers17">
+		<p  id="question" class="question17">17. goto can be used to jump from main() to within a function.</p>
+			<ul id="question" class="answers17">
 				<input type="radio" name="q17" value="1" id="q17a"><label for="q17a">true</label><br/>
 				<input type="radio" name="q17" value="2" id="q17b"><label for="q17b">false</label><br/>
 				<input type="radio" name="q17" value="3" id="q17c"><label for="q17c">depends</label><br/>
 				<input type="radio" name="q17" value="4" id="q17d"><label for="q17d"> varies</label><br/>
 			</ul> 
-		<p class="question18">18.Which keyword is used to come out of a loop only for that iteration?</p>
-			<ul class="answers18">
+		<p id="question" class="question18">18.Which keyword is used to come out of a loop only for that iteration?</p>
+			<ul id="question" class="answers18">
 				<input type="radio" name="q18" value="1" id="q18a"><label for="q18a">break</label><br/>
 				<input type="radio" name="q18" value="2" id="q18b"><label for="q18b">continue</label><br/>
 				<input type="radio" name="q18" value="3" id="q18c"><label for="q18c">return</label><br/>
 				<input type="radio" name="q18" value="4" id="q18d"><label for="q18d"> none of the mentioned</label><br/>
 			</ul> 
 
-		<p class="question19">19.The keyword ‘break’ cannot be simply used within ___</p>
-			<ul class="answers19">
+		<p id="question" class="question19">19.The keyword ‘break’ cannot be simply used within ___</p>
+			<ul id="question" class="answers19">
 				<input type="radio" name="q19" value="1" id="q19a"><label for="q19a">do-while</label><br/>
 				<input type="radio" name="q19" value="2" id="q19b"><label for="q19b"> if-else</label><br/>
 				<input type="radio" name="q19" value="3" id="q19c"><label for="q19c"> for</label><br/>
 				<input type="radio" name="q19" value="4" id="q19d"><label for="q19d"> while</label><br/>
 			</ul> 
-		<p class="question20">20.Which loop is most suitable to first perform the operation and then test the condition?</p>
-			<ul class="answers20">
+		<p id="question" class="question20">20.Which loop is most suitable to first perform the operation and then test the condition?</p>
+			<ul id="question" class="answers20">
 				<input type="radio" name="q20" value="1" id="q20a"><label for="q20a"> for loop</label><br/>
 				<input type="radio" name="q20" value="2" id="q20b"><label for="q20b">while loop</label><br/>
 				<input type="radio" name="q20" value="3" id="q20c"><label for="q20c"> do-while loop</label><br/>
